76e4b9763e27e0649a55463a5e67e32dc797f18d
Elisa Calderon <136856759+ElisaCalderon@users.noreply.github.com>
2024-01-22 19:17:57 -0600

    Ultimo commit

---
+++ bd/signup.go
@@ -0,0 +1,29 @@
+package bd
+
+import (
+	"fmt"
+
+	"github.com/ElisaCalderon/gambituser1/models"
+	"github.com/ElisaCalderon/gambituser1/tools"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func SignUp(sig models.SignUp) error {
+	fmt.Println("Comienza Registro")
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+
+	sentencia := "INSERT INTO users (User_Email, User_UUID, User_DateAdd) VALUES ('"+sig.UserEmail+"', '"+sig.UserUUID+"', '"+tools.FechaMySQL()+"')"
+	fmt.Println(sentencia)
+	_, err = Db.Exec(sentencia)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("SignUp > Ejecucion exitosa")
+	return nil
+}
